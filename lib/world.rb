d948fceb307179cd7c7a9f3850cd875b75636026
Kyle Geske <stungeye@gmail.com>
2014-12-17 16:20:29 -0600

    We can now request an empty world.

--- lib/world.rb
+++ lib/world.rb
@@ -1,4 +1,8 @@
 class World
+  def self.empty
+    new
+  end
+
   def empty?(if_empty)
     if_empty.call
     self
--- spec/world_spec.rb
+++ spec/world_spec.rb
@@ -2,23 +2,23 @@ require 'spec_helper'
 
 describe World do
   before(:each) do
-    @world = World.new
+    @empty_world = World.empty
   end
 
   it 'should correct initialize as a World' do
-    expect(@world.class).to eq(World)
+    expect(@empty_world.class).to eq(World)
   end
 
   it 'should be empty when first initialized' do
     mock = double
     expect(mock).to receive(:message)
-    @world.empty?(-> { mock.message })
+    @empty_world.empty?(-> { mock.message })
   end
 
   it 'an empty world should remain empty after one tick' do
     mock = double
     expect(mock).to receive(:message)
-    @world.tick
-    @world.empty?(-> { mock.message })
+    @empty_world.tick
+    @empty_world.empty?(-> { mock.message })
   end
 end
