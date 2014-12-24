dea23d5c081271591b337eb6174441ef85bf1454
Kyle Geske <stungeye@gmail.com>
2014-12-24 07:56:19 -0600

    Ignoring vim swps

--- lib/location.rb
+++ lib/location.rb
@@ -3,4 +3,8 @@ class Location
     @x = x
     @y = y
   end
+
+  def same_position?(other_location, if_same_position)
+    if_same_position.call
+  end
 end
--- spec/location_spec.rb
+++ spec/location_spec.rb
@@ -8,4 +8,12 @@ describe Location do
   it 'should correctly initialize as a Location' do
     expect(@location.class).to eq(Location)
   end
+
+  it 'should recognize locations which an identical position' do
+    first_location = Location.new(1, 2)
+    second_location = Location.new(1, 2)
+    same_position = false
+    first_location.same_position?(second_location, -> { same_position = true })
+    expect(same_position).to be true
+  end
 end
