02aa0f3ead4cd8af956b0204b110d0f840f8238c
Kyle Geske <stungeye@gmail.com>
2014-12-31 09:11:13 -0600

    Game.new is private now. Game.empty is used to create empty games.

--- lib/game.rb
+++ lib/game.rb
@@ -1,4 +1,11 @@
 class Game
+
+  private_class_method :new
+
+  def self.empty
+    new
+  end
+
   def output(ui)
   end
 end
--- spec/game_spec.rb
+++ spec/game_spec.rb
@@ -2,7 +2,7 @@ require 'spec_helper'
 
 describe Game do
   let(:ui) { double(:user_interface).as_null_object }
-  subject(:game) { Game.new }
+  subject(:game) { Game.empty }
 
   it 'starts with all cells dead' do
     expect(ui).to have_not_received(:draw_cell)
