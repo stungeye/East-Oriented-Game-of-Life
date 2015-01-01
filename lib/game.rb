7b83ce649080d5a1fcaf8922a334fd5d4e5e1e13
Kyle Geske <stungeye@gmail.com>
2015-01-01 15:10:56 -0600

    Fixed Board#points_surrounding to include diagonal cells, now returns 8 instead of four neighbours. This points to two things: 1) Testing missed this because the method is private. 2) All of Board's private methods could be moved to a location/coordinate value object.

--- lib/game.rb
+++ lib/game.rb
@@ -65,10 +65,9 @@ class Board
   private
 
   def points_surrounding(x, y)
-    [[x, y - 1],
-     [x - 1, y],
-     [x + 1, y],
-     [x, y + 1]].to_set
+    [[x - 1, y - 1], [x, y - 1], [x + 1, y - 1],
+     [x - 1, y    ],             [x + 1, y    ],
+     [x - 1, y + 1], [x, y + 1], [x + 1, y + 1]].to_set
   end
 
   def fringe
