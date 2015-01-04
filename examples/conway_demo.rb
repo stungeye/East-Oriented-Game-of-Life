6f60001440bded6ba51c4c8ebf9a7628364e8cdc
Kyle Geske <stungeye@gmail.com>
2015-01-04 08:22:02 -0600

    Modified conway demo to use the new RulesetWorldBuilder class.

--- examples/conway_demo.rb
+++ examples/conway_demo.rb
@@ -1,5 +1,7 @@
 require_relative '../lib/game_of_life.rb'
 
+ruleset = RulesetWorldBuilder.new(ConwayAliveRules, ConwayDeadRules)
+
 world = World.empty
 world_coords = [[1, 1], [1, 2], [1, 3], [5, 5], [5, 6], [6, 5], [6, 6]]
 world_coords.each do |x, y|
@@ -10,8 +12,5 @@ end
   ui = UserInterface2D.new(10, 10)
   world.output(ui)
   ui.print_world
-  new_world = World.empty
-  world.apply_alive_rules(ConwayAliveRules.new(new_world))
-  world.apply_dead_rules(ConwayDeadRules.new(new_world))
-  world = new_world
+  world = ruleset.next_world(world)
 end
