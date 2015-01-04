1a948cc4aac1c9e4ba37476230034ae4da97fda1
Kyle Geske <stungeye@gmail.com>
2015-01-04 07:11:14 -0600

    Spec for Coordinate2D

--- spec/game_spec.rb
+++ spec/game_spec.rb
@@ -105,3 +105,20 @@ describe ConwayDeadRules do
     dead_rules.apply(dead_cell, 4)
   end
 end
+
+describe Coordinate2D do
+  subject(:coordinate2D) { Coordinate2D.new(1, 1) }
+  describe '#neighbouring_coordinates' do
+    it 'does not include itself in the neighbourhood' do
+      expect(coordinate2D.neighbouring_coordinates).to_not include(coordinate2D)
+    end
+  
+    it 'includes the correct neighbours' do
+      expect(coordinate2D.neighbouring_coordinates).to include(
+        Coordinate2D.new(0, 0), Coordinate2D.new(0, 1), Coordinate2D.new(0, 2),
+        Coordinate2D.new(1, 0), Coordinate2D.new(1, 2),
+        Coordinate2D.new(2, 0), Coordinate2D.new(2, 1), Coordinate2D.new(2, 2)
+      )
+    end
+  end
+end
